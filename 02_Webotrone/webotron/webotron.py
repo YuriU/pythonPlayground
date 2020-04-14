441ae65ffc49d76222573abda094bbe4c0f165b7
YuriU <jerryhire8@gmail.com>
2020-04-14 23:30:30 +0300

    Added upload logic

--- 02_Webotrone/webotron/webotron.py
+++ 02_Webotrone/webotron/webotron.py
@@ -75,17 +75,28 @@ def setup_bucket(bucket):
 
     return
 
+def upload_file(s3_bucket, path, key):
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': 'text/html'
+        }
+    )
+
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
-def sync(pathname):
+@click.argument('bucket')
+def sync(pathname, bucket):
     "Sync content of PATHNAME to BUCKET"
 
+    s3_bucket = s3.Bucket(bucket)
     root = Path(pathname).expanduser().resolve()
 
     def handle_directory(target):
         for p in target.iterdir():
             if p.is_dir(): handle_directory(p)
-            if p.is_file(): print("Path: {}\n Key: {}".format(p, p.relative_to(root)))
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
 
     handle_directory(root)
 
