4330929368ccba9819d4b6dc49ae4596771c0ee9
YuriU <jerryhire8@gmail.com>
2020-04-16 16:40:56 +0300

    Added final version

--- 03_Notifon/notifier/handler.py
+++ 03_Notifon/notifier/handler.py
@@ -1,11 +1,12 @@
 import os
+import requests
 
 
 def post_to_slack(event, context):
     slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
 
-    print(slack_webhook_url)
-    print(event)
+    data = { "text": "An event happened."}
+    requests.post(slack_webhook_url, json=data)
 
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
