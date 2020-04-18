7bd457ac8c7aa706a8d4db75068df00b4a842720
YuriU <jerryhire8@gmail.com>
2020-04-18 11:17:34 +0300

    Added rekognition response handler

--- 04_Videolyzer/videolyzer/handler.py
+++ 04_Videolyzer/videolyzer/handler.py
@@ -1,5 +1,29 @@
 import urllib
 import boto3
+import os
+import json
+
+def get_video_labels(job_id):
+    rekognition_client = boto3.client('rekognition')
+
+    response = rekognition_client.get_label_detection(JobId=job_id)
+
+    next_token = response.get('NextToken', None)
+
+    while next_token:
+        next_page = rekognition_client.get_label_detection(
+            JobId=job_id,
+            NextToken=next_token
+        )
+
+        next_token = next_page.get('NextToken', None)
+
+        response['Labels'].extend(next_page['Labels'])
+
+    return response
+
+def put_labels_in_db(data, video_name, video_bucket):
+    pass
 
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
@@ -9,6 +33,10 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket, 
                 'Name': key 
                 }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
 
     print(response)
@@ -24,5 +52,17 @@ def start_processing_video(event, context):
         )
 
     print(event)
+
+    return
+
+def handle_label_detection(event, context):
+    for record in event['Records']:
+        message = json.loads(record['Sns']['Message'])
+        job_id = message['JobId']
+        s3_object = message['Video']['S3ObjectName']
+        s3_bucket = message['Video']['S3Bucket']
+        response = get_video_labels(job_id)
+        print(response)
+        put_labels_in_db(response, s3_object, s3_bucket)
 
     return
